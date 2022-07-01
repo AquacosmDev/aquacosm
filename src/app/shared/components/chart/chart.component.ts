fb5597f6b5a7ae1b8d14e560448a330fdd7404f8
niekvanrabenswaaij <niekvrabenswaaij@gmail.com>
2022-07-01 11:49:33 +0200

    Fix time axis bug - p2

--- src/app/shared/components/chart/chart.component.ts
+++ src/app/shared/components/chart/chart.component.ts
@@ -91,7 +91,6 @@ export class ChartComponent implements OnInit, OnChanges {
         }),
         labels: this.getChartLabels(startTime, endTime)
       };
-      console.log(this.lineChartData);
     }
   }
 
